43f335e0464c0f1055077b0c4d392eb2021a7897
tallyb <tally.barak@gmail.com>
2020-03-18 20:18:55 +0200

    angular 9 fixes

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -28,8 +28,7 @@
  * are required for JIT compilation.  StackBlitz examples are
  * compiled using JIT.
  */
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
